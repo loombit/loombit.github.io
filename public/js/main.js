4410a2c3ece0da199d2984c3a1537715d7067cb4
Latrasis <jacobapayne@gmail.com>
2014-08-02 18:47:31 -0700

    Desktop 1/2

--- public/js/main.js
+++ public/js/main.js
@@ -4,7 +4,7 @@ $(window).resize(function() {
 
 	var height = $(window).height();
 	height = height + -$('navbar.navbar-default').height();
-	$('section').height(height);
+	$('#wheight').height(height);
 
 }).resize();
 
@@ -17,20 +17,10 @@ var
 		.attr('height', '100%')
 		.attr('width', '100%')
 		.style('position', 'absolute')
-		.style('top', '0'),
+		.style('top', '0')
+		.style('left','0'),
 
 	logoMobile = frontcanvas.select('object.logo'),
-
-	circleData = [0, 1, 2, 3],
-	circlegroup = frontsvg.append('g')
-		.style('opacity', 0),
-	circles = circlegroup.selectAll('circle')
-		.data(circleData)
-		.enter()
-		.append('circle')
-		.attr('cx', function(d) {return d*30+ '%';})
-		.attr('r', 25)
-		.style('fill', '#000'),
 
 	infotext = frontsvg.append('text')
 		.attr('x', '10%')
@@ -68,23 +58,5 @@ $('#toggleAbout').on('click', function(event) {
 $('#toggle').on('click', function () {
 	$(this).parents('section').fadeToggle('300');
 });
-
-$(window).on('mousemove', function(event) {
-
-	circlegroup.transition()
-		.attr('transform',function() {
-			return 'translate('+ (event.pageX*0.1)+',' +(event.pageY*0.1) +')';
-		})
-		.style('opacity', 0.4)
-		.duration(3000)
-		.ease('elastic');
-
-
-	infotext.text(function() {
-		return 'X: ' + event.pageX +' Y: ' + event.pageY ;
-	});
-
-});
-
 
 
